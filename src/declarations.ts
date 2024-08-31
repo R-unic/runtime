9440d77cb3641c0162d2118bc81b13db414b6fa9
Tesmi-Devlop <sashdfr@gmail.com>
2024-09-01 04:40:32 +1000

    feat: added Method

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,8 +1,20 @@
 export class Type {
 	public readonly Name!: string;
 	public readonly FullName!: string;
+	public readonly BaseType: Type | undefined;
 	public readonly Interfaces!: Type[];
 	public readonly Properties!: Property[];
+	public readonly Methods!: Method[];
+}
+
+export class Method {
+	readonly Name!: string;
+	readonly ReturnType!: Type;
+	readonly Parameters!: Type[];
+	readonly AccessModifier!: number;
+	readonly IsStatic!: boolean;
+	readonly IsAbstract!: boolean;
+	//readonly Callback!: (context: unknown, ...args: unknown[]) => unknown;
 }
 
 export class Property {
